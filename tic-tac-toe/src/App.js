a9eb5bfdde30860e5defdcda32f449a66893bd01
heesun <heesun9793@naver.com>
2023-07-16 01:51:05 +0900

    tutorial 완성

--- tic-tac-toe/src/App.js
+++ tic-tac-toe/src/App.js
@@ -1,5 +1,126 @@
 import React from 'react';
+import { useState } from 'react';
 
-export default function Square() {
-  return <button className="square">X</button>;
+
+export default function Game() {
+  const [history, setHistory] = useState([Array(9).fill(null)]);
+  const [currentMove, setCurrentMove] = useState(0);
+  const xIsNext = currentMove % 2 === 0;
+  const currentSquares = history[currentMove];
+
+  function handlePlay(nextSquares) {
+    const nextHistory = [...history.slice(0, currentMove + 1), nextSquares];
+    setHistory(nextHistory);
+    setCurrentMove(nextHistory.length - 1);
+  }
+
+  function jumpTo(nextMove) {
+    setCurrentMove(nextMove);
+  }
+
+  const moves = history.map((squares, move) => {
+    let description;
+    if (move > 0) {
+      description = 'Go to move #' + move;
+    } else {
+      description = 'Go to game start';
+    }
+    return (
+      <li key={move}>
+        <button onClick={() => jumpTo(move)}>{description}</button>
+      </li>
+    );
+  });
+
+  return (
+    <div className="game">
+      <div className="game-board">
+        <Board xIsNext={xIsNext} squares={currentSquares} onPlay={handlePlay} />
+      </div>
+      <div className="game-info">
+        <ol>{moves}</ol>
+      </div>
+    </div>
+  );
+}
+
+
+function Board({ xIsNext, squares, onPlay }) {
+
+  function handleClick(index) {
+
+    /*JavaScript에서 null, undefined, 0, 빈 문자열(''), false, NaN이 아닌 값들은 truthy로 간주*/
+    /*truthy란 조건식이 true로 평가되는 값*/
+    if (squares[index] || calculateWinner(squares)) {
+      return;
+    }
+    /*slice() 함수는 원본 배열 또는 문자열을 변경하지 않고 원하는 범위의 요소 또는 문자를 추출*/
+    /*slice(startIndex, endIndex) : endIndex는 추출 범위에 포함되지 않음*/
+    const nextSquares = squares.slice();
+    if (xIsNext) {
+      nextSquares[index] = "X";
+    } else {
+      nextSquares[index] = "O";
+    }
+    onPlay(nextSquares);
+  }
+
+  const winner = calculateWinner(squares);
+  let status;
+  if (winner) {
+    status = "Winner: " + winner;
+  } else {
+    status = "Next player: " + (xIsNext ? "X" : "O");
+  }
+
+
+  return (
+    <React.Fragment>
+      <div className="status">{status}</div>
+      <div className="board-row">
+        <Square value={squares[0]} onSquareClick={() => handleClick(0)} />
+        <Square value={squares[1]} onSquareClick={() => handleClick(1)} />
+        <Square value={squares[2]} onSquareClick={() => handleClick(2)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[3]} onSquareClick={() => handleClick(3)} />
+        <Square value={squares[4]} onSquareClick={() => handleClick(4)} />
+        <Square value={squares[5]} onSquareClick={() => handleClick(5)} />
+      </div>
+      <div className="board-row">
+        <Square value={squares[6]} onSquareClick={() => handleClick(6)} />
+        <Square value={squares[7]} onSquareClick={() => handleClick(7)} />
+        <Square value={squares[8]} onSquareClick={() => handleClick(8)} />
+      </div>
+    </React.Fragment>
+  );
+}
+
+function Square({ value, onSquareClick }) {
+
+  // const [value, setValue] = useState(null);/
+
+
+  return <button className="square" onClick={onSquareClick}>{value}</button>;
+}
+
+function calculateWinner(squares) {
+  const lines = [
+    [0, 1, 2],
+    [3, 4, 5],
+    [6, 7, 8],
+    [0, 3, 6],
+    [1, 4, 7],
+    [2, 5, 8],
+    [0, 4, 8],
+    [2, 4, 6]
+  ];
+  for (let i = 0; i < lines.length; i++) {
+    const [a, b, c] = lines[i];
+    /* === : 값과 타입이 모두 일치해야만 결과가 true로 평가*/
+    if (squares[a] && squares[a] === squares[b] && squares[a] === squares[c]) {
+      return squares[a];
+    }
+  }
+  return null;
 }
--- tic-tac-toe/src/index.js
+++ tic-tac-toe/src/index.js
@@ -4,6 +4,7 @@ import "./styles.css";
 
 import App from "./App";
 
+/*해당 파일은, App.js 파일에서 만든 구성 요소와 웹 브라우저 (index.html 파일)사이의 브리지 역할*/
 const root = createRoot(document.getElementById("root"));
 root.render(
   <StrictMode>
